d8efa2b767626f49f703699832cfe21f32c9955c
Harshalyadav <shailyadav3166@gmail.com>
2022-05-21 12:46:35 +0530

    add password

--- client/src/components/LoginModel/Loginmodel.components.jsx
+++ client/src/components/LoginModel/Loginmodel.components.jsx
@@ -22,18 +22,20 @@ const LoginModel = ({setShowModel , isSignUp })=>{
     return (
         <>
                <div className="absloute h-600 my-auto mx-auto top-8 right-0 left-0 p-10 max-w-prose bg-white">
-                   <div className="float-right">
-                       <h2 onClick={handelClick} className="">ⓧ</h2>
+                   
+                       <div onClick={handelClick} className="float-right text-5xl  p-2 hover:cursor-pointer">ⓧ</div>
+
+                       <h2 className="text-3xl text-gray-800 m-2">{isSignUp ? 'CREATE ACCOUNT': 'LOG IN'}</h2>
                        <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
 
                        <form onSubmit={handelSubmit}
-                       className="">
+                       className=" flex flex-col p-4 m-4">
 
                            <input 
                               type ="email"
-                              id="email"
+                               className="m-2 p-2 text-md focus:outline-none"
                               name="email"
-                              placholder="email"
+                             placeholder="abcd@email.com"
                               required= {true}
                               onChange={(e)=> setEmail(e.target.value)}
                            />
@@ -41,33 +43,38 @@ const LoginModel = ({setShowModel , isSignUp })=>{
                            <input 
                                type ="password"
                                id ="password"
+                               className="m-2 p-2 text-md focus:outline-none"
                                name ="password"
                                placeholder="password"
                                required= {true}
                                onChange={(e)=>setPassword(e.target.value)}
                            />
-                           <input 
+                          { isSignUp && <input 
                               type = "password"
                               id = "password-confirm"
+                              className="m-2 p-2 text-md  focus:outline-none"
                               name = "password-confirm"
                               placeholder = "confirm-password"
                               required= {true}
                               onChange= {(e)=>setConfirmPassword(e.target.value)}
                            
-                           />
+                           />}
                            <input  
                                 type="submit"
-                                className=""
+                                className=" text-gray-600 text-md font-semibold px-2 py-4 rounded-md border-solid border-2 border-gray-500 m-4 hover:text-gray-800 hover:border-gray-800
+                                hover:border-2
+                                hover:border-solid"
                            
                            /> 
                            <p> {error} </p>
                             
                         
                        </form>
-                        <h2> Get the App </h2>                       
+                       <hr/>
+                        <h2 className="m-3 text-lg"> Get the App </h2>                       
                    </div>
 
-               </div>
+               
 
 
         </>
